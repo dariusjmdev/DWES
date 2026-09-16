e9c86ee7350b1229a51067d198c6fc2589b07d60
dariusjmdev <marinescu.dariusjoanito@jgvaldemora.org>
2026-09-16 10:18:07 +0200

    Ejercicio tercera clase

--- src/main/java/DWES/Main.java
+++ src/main/java/DWES/Main.java
@@ -1,49 +1,31 @@
 package DWES;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        String idIncidencia = "INC001";
+        String titulo = "No funciona el proyector";
+        String aula = "Aula 101";
+        String profesor = "Juan Pérez";
+        String prioridad = "Alta";
+        String estado = "Abierta";
+        String centro = "IES JGValdemora";
+        String departamento = "Informática";
+        String anioCurso = "2026/2027";
+        String version = "1.0";
+        String tecnico = "Javier Doñoro";
+        int incidenciass_abiertas = 5;
 
-        System.out.println("Buenos Días!!");
-
-        System.out.println("Introduce tu nombre: ");
-        String nombre = sc.nextLine();
-
-        System.out.println("Introduce el número de incidencias abiertas: ");
-        int incidencias_abiertas = sc.nextInt();
-
-        if (incidencias_abiertas < 0) {
-            System.out.println("El número de incidencias abiertas no puede ser negativo.");
-            return;
+        System.out.println("ID Incidencia: " + idIncidencia);
+        System.out.println("Título: " + titulo);
+        System.out.println("Aula: " + aula);
+        System.out.println("Profesor: " + profesor);
+        System.out.println("Prioridad: " + prioridad);
+        System.out.println("Estado: " + estado);
+        System.out.println("Centro: " + centro);
+        System.out.println("Departamento: " + departamento);
+        System.out.println("Año Curso: " + anioCurso);
+        System.out.println("Versión: " + version);
+        System.out.println("Técnico: " + tecnico);
+        System.out.println("Incidencias Abiertas: " + incidenciass_abiertas);
         }
-
-        System.out.println("Introduce el número de incidencias cerradas: ");
-        int incidencias_cerradas = sc.nextInt();
-        if (incidencias_cerradas < 0) {
-            System.out.println("El número de incidencias cerradas no puede ser negativo.");
-            return;
-        }
-        int incidencias_totales = incidencias_abiertas + incidencias_cerradas;
-
-        resumenDeveloper(nombre);
-        avisoIncidencias(incidencias_abiertas, incidencias_cerradas, incidencias_totales);
-
-        sc.close();
-    }
-
-    public static void avisoIncidencias(int incidencias_abiertas, int incidencias_cerradas, int incidencias_totales) {
-        if (incidencias_abiertas == 0) {
-            System.out.println("Todo en orden");
-        } else {
-            System.out.println("Hay incidencias pendientes!!!!");
-        }
-        System.out.println("Número de incidencias abiertas: " + incidencias_abiertas);
-        System.out.println("Número de incidencias cerradas: " + incidencias_cerradas);
-        System.out.println("Número de incidencias totales: " + incidencias_totales);
-    }
-    public static void resumenDeveloper(String nombre) {
-        System.out.println("Técnico " + nombre);
-    }
 }
