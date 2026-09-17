377499b50a90fe8217d5a6b38ddc124c4f73e8d1
dariusjmdev <marinescu.dariusjoanito@jgvaldemora.org>
2026-09-17 11:56:53 +0200

    Clase 4, introducción a Scanner

--- src/main/java/DWES/Main.java
+++ src/main/java/DWES/Main.java
@@ -1,31 +1,26 @@
 package DWES;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String idIncidencia = "INC001";
-        String titulo = "No funciona el proyector";
-        String aula = "Aula 101";
-        String profesor = "Juan Pérez";
-        String prioridad = "Alta";
-        String estado = "Abierta";
-        String centro = "IES JGValdemora";
-        String departamento = "Informática";
-        String anioCurso = "2026/2027";
-        String version = "1.0";
-        String tecnico = "Javier Doñoro";
-        int incidenciass_abiertas = 5;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("ID Incidencia: " + idIncidencia);
+        System.out.print("Ingrese el título: ");
+        String titulo = scanner.nextLine();
+        System.out.print("Ingrese una descripción de la incidencia: ");
+        String descripcion = scanner.nextLine();
+        System.out.print("Ingrese el nombre de la aula: ");
+        String aula = scanner.nextLine();
+        System.out.print("Ingrese el nombre del profesor: ");
+        String profesor = scanner.nextLine();
+        System.out.print("Ingrese el número de equipos afectados: ");
+        int numEquipos = Integer.parseInt(scanner.nextLine());
+
         System.out.println("Título: " + titulo);
+        System.out.println("Descripción: " + descripcion);
         System.out.println("Aula: " + aula);
         System.out.println("Profesor: " + profesor);
-        System.out.println("Prioridad: " + prioridad);
-        System.out.println("Estado: " + estado);
-        System.out.println("Centro: " + centro);
-        System.out.println("Departamento: " + departamento);
-        System.out.println("Año Curso: " + anioCurso);
-        System.out.println("Versión: " + version);
-        System.out.println("Técnico: " + tecnico);
-        System.out.println("Incidencias Abiertas: " + incidenciass_abiertas);
-        }
+        System.out.println("Equipos afectados: " + numEquipos);
+    }
 }
