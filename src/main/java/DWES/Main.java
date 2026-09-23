11b593c335ee0169c6bde29fd51b75baed543100
dariusjmdev <marinescu.dariusjoanito@jgvaldemora.org>
2026-09-23 10:32:09 +0200

    Clase 7 y 8; añadido menú(Clase7) con do-while(Clase8).

--- src/main/java/DWES/Main.java
+++ src/main/java/DWES/Main.java
@@ -4,37 +4,53 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        mostrarmenu();
+    }
+    public static void mostrarmenu() {
         Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("1. Registrar incidencia");
+            System.out.println("2. Consultar incidencias");
+            System.out.println("3. Mostrar estadisticas");
+            System.out.println("4. Información del programa");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine(); // Limpiar el buffer de entrada
 
-        System.out.print("Introduce el titulo: ");
-        String titulo = sc.nextLine();
-
-        System.out.print("Introduce la descripción del probema: ");
-        String descripcion = sc.nextLine();
-
-        System.out.print("Introduce el número de equipos afectados: ");
-        int numEquipos = sc.nextInt();
-
-        String prioridad = establecerPrioridad(numEquipos);
-
-        System.out.println("Título: " + titulo);
-        System.out.println("Descripción: " + descripcion);
-        System.out.println("Número de equipos afectados: " + numEquipos);
-        System.out.println("Prioridad: " + prioridad);
-
+            switch (opcion) {
+                case 1:
+                    System.out.print("Introduce el titulo de la incidencia: ");
+                    String titulo = sc.nextLine();
+                    System.out.print("Introduce el aula de la incidencia:");
+                    String aula = sc.nextLine();
+                    System.out.print("Introduce el número de equipos afectados: ");
+                    int numEquipos = sc.nextInt();
+                    sc.nextLine(); // Limpiar el buffer de entrada
+                    System.out.println("Incidencia registrada: " + titulo + ", Aula: " + aula + ", Número de equipos afectados: " + numEquipos);
+                    break;
+                case 2:
+                    System.out.println("Modulo de consulta en desarrollo...");
+                    break;
+                case 3:
+                    System.out.println("Introduce el número de incidencias abiertas: ");
+                    int incidenciasAbiertas = sc.nextInt();
+                    System.out.println("Introduce el número de incidencias cerradas: ");
+                    int incidenciasCerradas = sc.nextInt();
+                    System.out.println("Estadísticas: Total de Incidencias : " + incidenciasAbiertas + " + " + incidenciasCerradas + " = " + (incidenciasAbiertas + incidenciasCerradas));
+                    break;
+                case 4:
+                    System.out.println("Información del programa HelpDesk: Versión 0.1. Curso: 2026-2027");
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
+        }while (opcion != 0);
     }
-    public static String establecerPrioridad(int numEquipos) {
-        if (numEquipos < 0) {
-            return "Número de equipos no puede ser negativo";
-        } else if (numEquipos == 1) {
-            return "Baja";
-        } else if (2 <= numEquipos && numEquipos <= 5) {
-            return "Media";
-        } else if (6 <= numEquipos && numEquipos <= 15) {
-            return "Alta";
-        } else {
-            return "Crítica";
-        }
-    }
+
 
 }
